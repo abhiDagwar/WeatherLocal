d2fe880e690ee832757f495b2b888f309fc194f3
Abhishek Dagwar <abhi.dagwar@gmail.com>
2022-03-30 18:30:51 +0530

    Clean code
    remove unnecessary code

--- WeatherLocal/Services/Endpoint.swift
+++ WeatherLocal/Services/Endpoint.swift
@@ -7,32 +7,6 @@
 
 import Foundation
 
-//enum Endpoints {
-//    static let baseUrl = "api.openweathermap.org"
-//    static let APIKey = "be75b4f8c97dad04380bbf70c292dc5b"
-//    //https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-//    case getGeoCodingAddressPath
-//
-//    var path: String {
-//        switch self {
-//        case .getGeoCodingAddressPath:
-//            return "/geo/1.0/direct"
-//        }
-//    }
-//
-//    var url: URL {
-//        return URL(string: path)!
-//    }
-//}
-
-//static let APIKey = "be75b4f8c97dad04380bbf70c292dc5b"
-
-enum Sorting: String {
-    case numberOfStars = "stars"
-    case numberOfForks = "forks"
-    case recency = "updated"
-}
-
 struct Endpoint {
     let path: String
     let queryItems: [URLQueryItem]
@@ -53,15 +27,6 @@ extension Endpoint {
 }
 
 extension Endpoint {
-//    static func geoCoding(for location: String) -> Endpoint {
-//        return Endpoint(
-//            path: "geo/1.0/direct",
-//            queryItems: [
-//                URLQueryItem(name: "q", value: location),
-//                URLQueryItem(name: "appid", value: "be75b4f8c97dad04380bbf70c292dc5b")
-//            ]
-//        )
-//    }
     static func search(matching query: String) -> Endpoint {
         return Endpoint(
             path: "/geo/1.0/direct",
